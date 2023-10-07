ac461cad2746d9156504029bd73ad85c8595ca09
Tucker Craig <tuckermcraig@gmail.com>
2023-10-07 19:38:29 -0400

    sends back json

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,21 +8,21 @@ export class AppController {
   // Use @Get() decorator to specify that this method should handle HTTP GET requests
   @Get()
   // Define a method to handle those GET requests. The method name 'logRequest' is arbitrary.
-  logRequest(@Req() request: Request): string {
+  logRequest(@Req() request: Request): any {
     // @Req() decorator injects the Express Request object
     console.log('Received a GET request'); // Log the type of request received
-    return 'Check your console for GET!'; // Send a response back to the client
+    return { message: 'Check your console for GET!' }; // Send a response back to the client
   }
 
   // Use @Post() decorator to specify that this method should handle HTTP POST requests
   @Post()
   // Define another method to handle POST requests.
-  logPostRequest(@Body() data: any, @Req() request: Request): string {
+  logPostRequest(@Body() data: any, @Req() request: Request): any {
     // @Body() decorator extracts the request payload (or "body")
     // @Req() decorator injects the Express Request object again, just like in logRequest()
 
     console.log('Received a POST request'); // Log that a POST request was received
     console.log(`Request Data: ${JSON.stringify(data)}`); // Log the actual payload sent with the POST request
-    return 'Check your console for POST!'; // Send a response back to the client
+    return { message: 'Check your console for POST!' }; // Send a response back to the client
   }
 }
